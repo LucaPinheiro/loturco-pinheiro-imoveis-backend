635e38b29710897b78d5c3e09e215f5baff327d8
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2024-12-22 19:45:19 -0300

    add environment configuration class for managing application settings

---
+++ app/env.py
@@ -0,0 +1,22 @@
+import os
+from enum import Enum
+from dotenv import load_dotenv
+
+
+class StageEnum(Enum):
+    PROD = "prod"
+    HOM = "hom"
+    DEV = "dev"
+    TEST = "test"
+    
+
+class Env:
+    load_dotenv()
+    
+    STAGE: StageEnum = StageEnum(os.getenv("STAGE", StageEnum.TEST.value))
+    
+    DATABASE_URL: str = os.environ.get("DATABASE_URL")
+    
+    JWT_SECRET: str = os.environ.get("JWT_SECRET", 'secret')
+    
+    ENCRYPT_KEY: str = os.environ.get("ENCRYPT_KEY", '$2b$12$K2wnfHK/BhGEwMMMYPK9rO')
