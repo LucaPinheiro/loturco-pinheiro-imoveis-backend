3334d62d4fe824edce0c85276f5f77c5ef6031c8
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-19 18:55:54 -0300

    add Property model with various attributes for real estate listings

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Boolean, Float, Integer, JSON
 
 
 class Base(DeclarativeBase):
@@ -15,3 +16,38 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+
+
+class Property(Base):
+    __tablename__ = "properties"
+
+    id = Column(String, primary_key=True, index=True)
+    sub_locality = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    street = Column(String, nullable=False)
+    house_number = Column(String, nullable=False)
+    complement = Column(String)
+    postal_code = Column(String, nullable=False)
+    pool = Column(Boolean, default=False)
+    gourmet = Column(Boolean, default=False)
+    grill = Column(Boolean, default=False)
+    party_room = Column(Boolean, default=False)
+    sports_court = Column(Boolean, default=False)
+    gym = Column(Boolean, default=False)
+    sauna = Column(Boolean, default=False)
+    playground = Column(Boolean, default=False)
+    gallery = Column(JSON)
+    type = Column(String, nullable=False)
+    name = Column(String)
+    description = Column(String)
+    phone = Column(String)
+    email = Column(String)
+    area = Column(Float)
+    bedrooms = Column(Integer)
+    suites = Column(Integer)
+    bathrooms = Column(Integer)
+    parking_spaces = Column(Integer)
+    floor = Column(Integer)
+    price = Column(Float)
+    condominium = Column(Float)
+    iptu = Column(Float)
