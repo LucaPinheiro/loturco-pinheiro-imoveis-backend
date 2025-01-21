aedf6ed2c43a06bff43d6a61ebe55fe8ed38f9e3
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-20 22:53:54 -0300

    enhance database table creation with connection logging and improved error handling

--- app/helpers/functions.py
+++ app/helpers/functions.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from app.env import Env
-from app.models.models import Base
+from app.models.models import Base, User, Property
 
 
 def create_db_tables():
@@ -8,11 +8,16 @@ def create_db_tables():
     try:
         engine = create_engine(Env.DATABASE_URL)
 
+        print(f"Connecting to database at: {Env.DATABASE_URL}")
+        print("Tables registered in Base.metadata:", Base.metadata.tables.keys())
+
+        Base.metadata.drop_all(engine)
         Base.metadata.create_all(engine)
 
         print("Tables created successfully.")
     except Exception as e:
         print(f"An error occurred while creating tables: {e}")
-        
+
+
 if __name__ == "__main__":
     create_db_tables()
